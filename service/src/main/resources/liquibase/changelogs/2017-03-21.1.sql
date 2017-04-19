d14c474ae594964dbef5ccefeefdbe6ece8eea23
Dessalines <tyhou13@gmx.com>
2017-04-19 16:01:51 -0700

    Adding DB rollback.

--- service/src/main/resources/liquibase/changelogs/2017-03-21.1.sql
+++ service/src/main/resources/liquibase/changelogs/2017-03-21.1.sql
@@ -8,6 +8,21 @@ create table user_ (
     created timestamp default current_timestamp
 );
 
+--rollback drop table user_ cascade;
+
+-- Just used for persisting logins, in case they need to be loaded by other instances on startup
+CREATE TABLE login (
+    id bigserial primary key,
+    user_id bigint not null,
+    jwt VARCHAR(255) not null,
+    created timestamp default current_timestamp,
+    constraint fk1_login_user foreign key (user_id)
+        references user_ (id)
+        on update cascade on delete cascade
+);
+
+--rollback drop table login cascade;
+
 create table question_set (
     id bigserial primary key,
     user_id bigint not null,
@@ -18,20 +33,25 @@ create table question_set (
         on update cascade on delete cascade
 );
 
+--rollback drop table question_set cascade;
+
 create table question (
     id bigserial primary key,
     user_id bigint not null,
     title varchar(255) not null,
     question_set_id bigint not null,
+    expire_time timestamp default null,
     threshold smallint not null default 30,
     created timestamp default current_timestamp,
     constraint fk1_question_user foreign key (user_id)
         references user_ (id)
-        on update cascade on delete cascade
+        on update cascade on delete cascade,
     constraint fk2_question_question_set foreign key (question_set_id)
         references question_set (id)
-        on update cascade on delete cascade,
+        on update cascade on delete cascade
 );
+
+--rollback drop table question cascade;
 
 create table candidate (
     id bigserial primary key,
@@ -41,11 +61,13 @@ create table candidate (
     created timestamp default current_timestamp,
     constraint fk1_candidate_user foreign key (user_id)
         references user_ (id)
-        on update cascade on delete cascade
+        on update cascade on delete cascade,
     constraint fk2_candidate_question foreign key (question_id)
         references question_set (id)
-        on update cascade on delete cascade,
+        on update cascade on delete cascade
 );
+
+--rollback drop table candidate cascade;
 
 create table vote (
     id bigserial primary key,
@@ -61,4 +83,23 @@ create table vote (
         on update cascade on delete cascade
 );
 
+--rollback drop table vote cascade;
+
+create table comment (
+    id bigserial primary key,
+    user_id bigint not null,
+    question_set_id bigint not null,
+    comment text not null,
+    created timestamp default current_timestamp,
+    constraint fk1_question_user foreign key (user_id)
+        references user_ (id)
+        on update cascade on delete cascade,
+    constraint fk2_question_question_set foreign key (question_set_id)
+        references question_set (id)
+        on update cascade on delete cascade
+);
+
+--rollback drop table comment cascade;
+
+select * from candidate;
 
