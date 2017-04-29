ca7801f3411055e45f9117e2ae0511bf5a433b70
Dessalines <tyhou13@gmx.com>
2017-04-29 12:02:30 -0700

    Try using traviss env vars.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 if [ "$TRAVIS_BRANCH" == "master"  ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     export SSHPASS=$DEPLOY_PASS
+    echo $DEPLOY_USER
     sshpass -e ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST "source ~/.nvm/nvm.sh;cd ~/git/flowchat;git checkout master;git pull;./install_prod.sh"
 fi
