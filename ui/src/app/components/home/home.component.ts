831576853cc6d0148e2ddaa265c7a5d2f9e81d41
Tyler <happydooby@gmail.com>
2017-05-15 15:30:20 -0700

    Fix bug with poll creation.

--- ui/src/app/components/home/home.component.ts
+++ ui/src/app/components/home/home.component.ts
@@ -5,6 +5,8 @@ import {
 	PollService,
 } from '../../services';
 
+import * as Hashids from 'hashids';
+
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
@@ -12,17 +14,20 @@ import {
 })
 export class HomeComponent implements OnInit {
 
+	private hashids: any;
+
 	constructor(private pollService: PollService,
 		private route: ActivatedRoute,
 		private router: Router) { }
 
 	ngOnInit() {
+		this.hashids = new Hashids();
 	}
 
 	createPoll() {
 		this.pollService.createPoll().subscribe(p => {
-			console.log(p);
-			this.router.navigate(['/poll', p.id]);
+			let hashId = this.hashids.encode(p.id);
+			this.router.navigate(['/poll', hashId]);
 		});
 	}
 
