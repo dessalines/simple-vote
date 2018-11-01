937a3f6a955d233fd61725c4da8a5ffcea81363c
Tyler <happydooby@gmail.com>
2018-11-01 13:20:56 -0700

    Fixing dev and prod scripts.

--- install_prod.sh
+++ install_prod.sh
@@ -1,9 +1,9 @@
 # Building the front end
 cd ui
 yarn
-yarn build --prod --aot --build-optimizer
+yarn build
 cp -R dist/ ../service/src/main/resources
 
 # Building the back end
 cd ../service
-sh install.sh -ssl ~/keystore.jks
+sh install.sh
